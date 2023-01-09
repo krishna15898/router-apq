a5f9da9db9956f5b80b88462d48a93d49a92832c
Sri Krishna Sahoo <srikrishna.sahoo@hotstar.com>
2023-01-10 01:27:50 +0530

    bench updated

--- apollo-router/benches/my_benchmark.rs
+++ apollo-router/benches/my_benchmark.rs
@@ -1,8 +1,9 @@
 use std::sync::Arc;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion, BenchmarkId};
 
 use http::header::CONTENT_TYPE;
 use mime::APPLICATION_JSON;
+use apollo_router::graphql::Error;
 use apollo_router::graphql::Request;
 use apollo_router::graphql::Response;
 use apollo_router::services::SubgraphService;
@@ -13,10 +14,26 @@ use criterion::async_executor::FuturesExecutor;
 use tower::ServiceExt;
 use apollo_router::Context;
 use tokio::runtime::Runtime;
+use apollo_router::graphql;
+use http::StatusCode;
+use std::net::SocketAddr;
+use hyper::Body;
+use std::convert::Infallible;
+use hyper::service::make_service_fn;
+use axum::Server;
+use http::Uri;
+use http::header::HOST;
+use tower::service_fn;
+use std::str::FromStr;
+use std::time;
+use hyper::body::Buf;
 
-async fn make_call() {
-    let subgraph_service = SubgraphService::new("test", Some(false));
-    let resp = subgraph_service
+const PERSISTED_QUERY_KEY: &str = "persistedQuery";
+const PERSISTED_QUERY_NOT_FOUND_EXTENSION_CODE: &str = "PERSISTED_QUERY_NOT_FOUND";
+
+async fn make_call(service: &apollo_router::services::SubgraphService) {
+    // println!("sending reqst: {:?}", time::SystemTime::now());
+    let resp = service
         .clone()
         .oneshot(SubgraphRequest {
             supergraph_request: Arc::new(
@@ -27,26 +44,286 @@ async fn make_call() {
             ),
             subgraph_request: http::Request::builder()
                 .header(CONTENT_TYPE, APPLICATION_JSON.essence_str())
-                .uri("https://www.example.org/")
-                .body(Request::builder().query("query").build())
+                .uri(Uri::from_str(
+                    &format!("http://{}", SocketAddr::from_str("127.0.0.1:3333").unwrap())).unwrap()
+                )
+                .body(Request::builder().query("query(
+    $token: String
+    $country: String
+    $xHsPlatform: String
+    $paginationToken: String
+    $trayID: String
+    $clientCapabilities: String
+    $collectionType: CollectionType!
+    $dynamicCollectionTitle: String
+    $dynamicCollectionEngTitle: String
+) {
+    fetchContentCollection(
+        contentCollectionRequest: {
+            hsRequest: {
+                token: $token
+                countryCode: $country
+                platformCode: $xHsPlatform
+            }
+            clientCapabilities: $clientCapabilities
+            trayID: $trayID
+            paginationToken: $paginationToken
+            collectionType: $collectionType
+            dynamicCollectionParam: {
+                title: $dynamicCollectionTitle
+                engTitle: $dynamicCollectionEngTitle
+            }
+        }
+    ) {
+        __typename
+        ... on ContentCollectionResponseV2 {
+            paginationToken
+            trayId
+            title {
+                title {
+                    stringText
+                    stringId
+                    placeholderMap {
+                        key
+                        value
+                    }
+                }
+                subtitle{
+                    stringText
+                    stringId
+                    placeholderMap {
+                        key
+                        value
+                    }
+                }
+                constructionMeta {
+                    contentTitle
+                    contentImages {
+                        horizontalImage {
+                            height
+                            width
+                            url
+                        }
+                        verticalImage {
+                            height
+                            width
+                            url
+                        }
+                        titleImage {
+                            height
+                            width
+                            url
+                        }
+                        imageMasthead {
+                            url
+                        }
+                    }
+                }
+            }
+            items {
+                itemMeta {
+                    id
+                    isArchived
+                    coreAttributes {
+                        contentType
+                        entityType
+                        title
+                        description
+                        images {
+                            horizontalImage {
+                                height
+                                width
+                                url
+                            }
+                            verticalImage {
+                                height
+                                width
+                                url
+                            }
+                            titleImage {
+                                height
+                                width
+                                url
+                            }
+                            imageMasthead {
+                                url
+                            }
+                            alterHorizontalImage {
+                                height
+                                url
+                                width
+                            }
+                        }
+                        languages {
+                            code
+                            displayName
+                            name
+                        }
+                        genres
+                        ageRating
+                        studioName
+                        isPaid
+                        contentProvider
+                        trailers
+                    }
+                    channelName
+                    isBTV
+                    isLive
+                    showType
+                    duration
+                    year
+                    enrichedMeta {
+                        metaValues
+                        metaType
+                    }
+                    showSeasonCount
+                    showID
+                    parentInfo{
+                        contentId
+                        title
+                    }
+                    sportsInfo {
+                        isHighlight
+                        isLive
+                        isReplay
+                        matchID
+                        sportsType
+                        tournamentSeasonId
+                        tournamentSeasonName
+                    }
+                }
+                watchListStatus
+            }
+        }
+        ... on EditorialTrayResponseV2 {
+            trayTitle
+            trayID
+            paginationToken
+            editorialTrayItems {
+                collectionItem {
+                    id
+                    isArchived
+                    coreAttributes {
+                        contentType
+                        entityType
+                        title
+                        description
+                        images {
+                            horizontalImage {
+                                height
+                                width
+                                url
+                            }
+                            verticalImage {
+                                height
+                                width
+                                url
+                            }
+                            titleImage {
+                                height
+                                width
+                                url
+                            }
+                            imageMasthead {
+                                url
+                            }
+                        }
+                        languages {
+                            code
+                            displayName
+                            name
+                        }
+                        genres
+                        ageRating
+                        studioName
+                        isPaid
+                        contentProvider
+                        trailers
+                    }
+                    channelName
+                    isBTV
+                    showType
+                    duration
+                    year
+                    enrichedMeta {
+                        metaValues
+                        metaType
+                    }
+                    showSeasonCount
+                    showID
+                    parentInfo{
+                        contentId
+                        title
+                    }
+                }
+            }
+        }
+    }
+}
+").build())
                 .expect("expecting valid request"),
             operation_kind: OperationKind::Query,
             context: Context::new(),
         })
         .await
         .unwrap();
+    // println!("got response.: {:?}", time::SystemTime::now());
+}
+async fn emulate_expected_apq_enabled_configuration(socket_addr: SocketAddr) {
+    async fn handle(request: http::Request<Body>) -> Result<http::Response<Body>, Infallible> {
+        // println!("servr got req: {:?}", time::SystemTime::now());
+        let (_, body) = request.into_parts();
+        let graphql_request: Result<graphql::Request, &str> = hyper::body::to_bytes(body)
+            .await
+            .map_err(|_| ())
+            .and_then(|bytes| serde_json::from_reader(bytes.reader()).map_err(|_| ()))
+            .map_err(|_| "failed to parse the request body as JSON");
 
-    assert_eq!(resp.response.body().data, Some(Value::String(ByteString::from("test"))));
+        match graphql_request {
+            Ok(request) => {
+                if !request.extensions.contains_key(PERSISTED_QUERY_KEY) {
+                    panic!("persistedQuery expected when configuration has apq_enabled=true")
+                }
+
+                // println!("servr sen rsp: {:?}", time::SystemTime::now());
+                return Ok(http::Response::builder()
+                    .header(CONTENT_TYPE, APPLICATION_JSON.essence_str())
+                    .status(StatusCode::OK)
+                    .body(
+                        serde_json::to_string(&Response {
+                            data: Some(Value::String(ByteString::from("test"))),
+                            errors: vec![Error::builder()
+                                .message("Random message")
+                                .extension_code(PERSISTED_QUERY_NOT_FOUND_EXTENSION_CODE)
+                                .build()],
+                            ..Response::default()
+                        })
+                            .expect("always valid")
+                            .into(),
+                    )
+                    .unwrap());
+            }
+            Err(_) => {
+                panic!("invalid graphql request recieved")
+            }
+        }
+    }
+
+    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
+    let server = Server::bind(&socket_addr).serve(make_svc);
+    server.await.unwrap();
 }
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function(
-        "benchmark a call",
-        |b| {
+fn criterion_benchmark(c: &mut Criterion) {
+    let subgraph_service = &SubgraphService::new("test", Some(false));
+    c.bench_with_input(
+        BenchmarkId::new( "hello", "subgraph service"),
+        subgraph_service,
+        |b, subgraph_service| {
             b.to_async(FuturesExecutor).iter(|| async {
-                make_call().await;
-            });
-        }
+                // println!("before starts: {:?}", time::SystemTime::now());
+                make_call(subgraph_service).await;
+            }) }
     );
 }
 
